9a182c53eaca94297eab289267e8508682727481
Natàlia <natalia.cavi@gmail.com>
2020-04-04 10:54:54 +0200

    Refactored data variables

--- configuration/app.vue
+++ configuration/app.vue
@@ -22,7 +22,7 @@
                 <label class="label">Container</label>
               </div>
               <div class="field-body">
-                <input class="input" type="text" placeholder=".container" v-model="codeParts.container">
+                <input class="input" type="text" placeholder=".container" v-model="container">
               </div>
             </div>
             <!-- ACTION -->
@@ -31,7 +31,7 @@
                 <label class="label">Action</label>
               </div>
               <div class="field-body">
-                <textarea class="textarea" placeholder="console.log('Scrooolling!')" v-model="codeParts.action"></textarea>
+                <textarea class="textarea" placeholder="console.log('Scrooolling!')" v-model="action"></textarea>
               </div>
             </div>
             <!-- CONDITIONS -->
@@ -46,7 +46,7 @@
                       <div class="field tile is-child">
                         <label class="label">Directions</label>
                         <div class="select is-multiple">
-                          <select multiple size="3" v-model="codeParts.conditions.directions">
+                          <select multiple size="3" v-model="conditions.directions">
                             <option value="all">all</option>
                             <option value="vertical">vertical</option>
                             <option value="horizontal">horizontal</option>
@@ -59,14 +59,14 @@
                       </div>
                       <div class="field tile is-child">
                         <label class="label">Offset</label>
-                        <input class="input" type="number" placeholder="x" v-model="codeParts.conditions.offset.x">
-                        <input class="input" type="number" placeholder="y" v-model="codeParts.conditions.offset.y">
+                        <input class="input" type="number" placeholder="x" v-model="conditions.offset.x">
+                        <input class="input" type="number" placeholder="y" v-model="conditions.offset.y">
                       </div>
                     </div>
                     <div class="tile is-parent">
                       <div class="field tile is-child">
                         <label class="label">Custom</label>
-                        <textarea class="textarea" placeholder="return true" v-model="codeParts.conditions.custom"></textarea>
+                        <textarea class="textarea" placeholder="return true" v-model="conditions.custom"></textarea>
                       </div>
                     </div>
                   </div>
@@ -79,7 +79,7 @@
                 <label class="label">Throttling</label>
               </div>
               <div class="field-body">
-                <input class="input" type="number" placeholder="200" v-model="codeParts.throttling">
+                <input class="input" type="number" placeholder="200" v-model="throttling">
               </div>
             </div>
             <!-- ONCE -->
@@ -89,7 +89,7 @@
               </div>
               <div class="field-body">
                 <label class="checkbox">
-                  <input type="checkbox" v-model="codeParts.once">
+                  <input type="checkbox" v-model="once">
                 </label>
               </div>
             </div>
@@ -107,43 +107,39 @@
 export default {
   name: 'App',
   data: () => ({
-    codeParts: {
-      constructor: 'new Actonscroll()',
-      container: '',
-      action: '',
-      conditions: {
-        directions: [],
-        offset: {},
-        custom: '',
-      },
-      throttling: '',
-      once: '',
-      start: 'start()',
+    container: '',
+    action: '',
+    conditions: {
+      directions: [],
+      offset: {},
+      custom: '',
     },
+    throttling: '',
+    once: '',
   }),
   computed: {
     containerCall: function() {
-      return this.codeParts.container !== '' && `.container(document.querySelector('${this.codeParts.container}'))`;
+      return this.container !== '' && `.container(document.querySelector('${this.container}'))`;
     },
     actionCall: function() {
-      return this.codeParts.action !== '' && `.action((verifiedConditions) => {\n    ${this.codeParts.action}\n  })`;
+      return this.action !== '' && `.action((verifiedConditions) => {\n    ${this.action}\n  })`;
     },
     directionCondition: function() {
-      return this.codeParts.conditions.directions.length !== 0
-        && `directions: [${this.codeParts.conditions.directions.map(direction => `'${direction}'`).join(', ')}],`;
+      return this.conditions.directions.length !== 0
+        && `directions: [${this.conditions.directions.map(direction => `'${direction}'`).join(', ')}],`;
     },
     offsetCondition: function() {
-      return (this.codeParts.conditions.offset.x || this.codeParts.conditions.offset.y)
+      return (this.conditions.offset.x || this.conditions.offset.y)
         && 'offset: {\n      '
           .concat([
-            this.codeParts.conditions.offset.x && `x: ${this.codeParts.conditions.offset.x},`,
-            this.codeParts.conditions.offset.y && `y: ${this.codeParts.conditions.offset.y},`,
+            this.conditions.offset.x && `x: ${this.conditions.offset.x},`,
+            this.conditions.offset.y && `y: ${this.conditions.offset.y},`,
            ].filter(Boolean).join('\n      '))
           .concat('\n    }),');
     },
     customCondition: function() {
-      return this.codeParts.conditions.custom !== ''
-        && `custom: () => {\n      ${this.codeParts.conditions.custom}\n    },`;
+      return this.conditions.custom !== ''
+        && `custom: () => {\n      ${this.conditions.custom}\n    },`;
     },
     conditionsCall: function() {
       return (this.directionCondition || this.offsetCondition || this.customCondition)
@@ -152,20 +148,20 @@ export default {
           .concat('\n  }),');
     },
     throttlingCall: function() {
-      return this.codeParts.throttling !== '' && `.throttling(${this.codeParts.throttling})`;
+      return this.throttling !== '' && `.throttling(${this.throttling})`;
     },
     onceCall: function() {
-      return this.codeParts.once && `.once()`;
+      return this.once && `.once()`;
     },
     code: function() {
       return [
-        `(${this.codeParts.constructor})`,
+        `(new Actonscroll())`,
         this.containerCall,
         this.actionCall,
         this.conditionsCall,
         this.throttlingCall,
         this.onceCall,
-        `.${this.codeParts.start};`,
+        `.start();`,
       ].filter(Boolean).join('\n  ');
     },
   },
